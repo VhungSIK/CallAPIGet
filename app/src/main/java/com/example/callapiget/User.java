9ecdb3cd0d495401865cfcb7c98d2a321a127fb9
VhungSIK <hungyn12.34n@gmail.com>
2023-09-23 03:51:06 +0700

    them User

--- app/src/main/java/com/example/callapiget/User.java
+++ app/src/main/java/com/example/callapiget/User.java
@@ -1,8 +1,51 @@
 package com.example.callapiget;
 
+import java.util.List;
+
 public class User {
     private int id;
     private String name;
     private boolean isActive;
     private Job job;
+    private List<Favorite> favorite;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+    }
+
+    public List<Favorite> getFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(List<Favorite> favorite) {
+        this.favorite = favorite;
+    }
 }

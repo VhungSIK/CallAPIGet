ef8c5555c12b80743ed2ff3636f46e7e061fb726
VhungSIK <hungyn12.34n@gmail.com>
2023-09-23 03:45:09 +0700

    them job

---
+++ app/src/main/java/com/example/callapiget/Job.java
@@ -0,0 +1,22 @@
+package com.example.callapiget;
+
+public class Job {
+    private int id;
+    private String job;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+}
--- app/src/main/java/com/example/callapiget/User.java
+++ app/src/main/java/com/example/callapiget/User.java
@@ -1,4 +1,7 @@
 package com.example.callapiget;
 
 public class User {
+    private int id;
+    private String name;
+    private boolean isActive;
 }

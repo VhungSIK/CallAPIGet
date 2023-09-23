12fa6686afe5f1e9715f48d5fe135d85e9d50375
VhungSIK <hungyn12.34n@gmail.com>
2023-09-23 17:33:26 +0700

    call api

--- app/src/main/java/com/example/callapiget/MainActivity.java
+++ app/src/main/java/com/example/callapiget/MainActivity.java
@@ -3,16 +3,65 @@ package com.example.callapiget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.callapiget.api.ApiService;
+import com.example.callapiget.model.Currency;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+    private TextView tvTerms;
+    private TextView tvSource;
+    private TextView tvUsdVnd;
+    private Button btnCallApi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvTerms = findViewById(R.id.tv_terms);
+        tvSource = findViewById(R.id.tv_source);
+        tvUsdVnd = findViewById(R.id.tv_usd_vnd);
+        btnCallApi = findViewById(R.id.bt_call_api);
+
+        btnCallApi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickCallApi();
+            }
+        });
+    }
+
+    private void clickCallApi() {
+        //Link Api: http://apilayer.net/api/live?access_key=843d4d34ae72b3882e3db642c51e28e6&currencies=VND&source=USD&format=1
+        ApiService.apiService.convertUsdToVnd("843d4d34ae72b3882e3db642c51e28e6", "VND",1).enqueue(new Callback<Currency>() {
+            @Override
+            public void onResponse(Call<Currency> call, Response<Currency> response) {
+                Toast.makeText(MainActivity.this, "Call Api success", Toast.LENGTH_LONG).show();
+
+                Currency currency = response.body();
+                if (currency != null && currency.isSuccess()) {
+                    tvTerms.setText(currency.getTerms());
+                    tvSource.setText(currency.getSource());
+                    tvUsdVnd.setText(String.valueOf(currency.getQuotes().getUSDVND()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Currency> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Call Api Error", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
